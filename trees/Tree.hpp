98f20f8c965f90d3c6cebfd89841c45fd865526d
Oustaa <otailaba98@gmail.com>
2025-12-24 21:30:35 +0100

    completing trees DSA section

--- trees/Tree.hpp
+++ trees/Tree.hpp
@@ -5,7 +5,7 @@ template<typename T>
 
 class Tree{
 public:
-  TreeNode<T> root;
+  TreeNode<T> *root;
 
   void create(){
     Queue<TreeNode<int> *> q;
@@ -25,6 +25,7 @@ public:
       cin >> userValue;
       if (userValue != -1) {
         TreeNode<int> *rChild = new TreeNode<int>(userValue);
+        currentNode->right = rChild;
         q.enqueue(rChild);
       }
 
@@ -33,8 +34,63 @@ public:
       cin >> userValue;
       if (userValue != -1) {
         TreeNode<int> *lChild = new TreeNode<int>(userValue);
+        currentNode->left = lChild;
         q.enqueue(lChild);
       }
     }
   }
+
+  static int leafNodes(TreeNode<int> * node) {
+    if (node == nullptr)
+      return 0;
+
+    if (!node->left && !node->right) return 1;
+
+    return leafNodes(node->right) + leafNodes(node->left); 
+  }
+
+  static int externalNodes(TreeNode<int> * node) {
+    if (node) {
+      if (node->left || node->right) 
+        return 
+          leafNodes(node->right) + 
+          leafNodes(node->left) + 1; 
+    }
+
+    return 0;
+  }
+
+  static int height(TreeNode<int> * node) {
+    if (node == nullptr)
+      return 0;
+
+    int x = height(node->left);
+    int y = height(node->right);
+
+    return (x > y ? x : y) + 1;
+  }
+
+  static void printPreOrder(TreeNode<int> * root) {
+    if (root) {
+      cout << root->value << ", ";
+      printPreOrder(root->right);
+      printPreOrder(root->left);
+    }
+  } 
+
+  static void printInOrder(TreeNode<int> * root) {
+    if (root) {
+      printInOrder(root->right);
+      cout << root->value << ", ";
+      printInOrder(root->left);
+    }
+  } 
+
+  static void printPostOrder(TreeNode<int> * root) {
+    if (root) {
+      printPostOrder(root->right);
+      printPostOrder(root->left);
+      cout << root->value << ", ";
+    }
+  } 
 };
--- trees/tree.cpp
+++ trees/tree.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 int main () {
-  Tree<int> *tree;
+  Tree<int> tree;
 
-  tree->create();
+  tree.create();
+  cout << "Pre Order: ";
+  Tree<int>::printPreOrder(tree.root);
+  cout << endl;
+  cout << "In Order: ";
+  Tree<int>::printInOrder(tree.root);
+  cout << endl;
+  cout << "Post Order: ";
+  Tree<int>::printPostOrder(tree.root);
+  cout << endl;
+  cout << "tree level is: " << Tree<int>::height(tree.root) << endl;
+  cout << "tree leaf nodes are: " << Tree<int>::leafNodes(tree.root) << endl;
+  cout << "tree external nodes are: " << Tree<int>::externalNodes(tree.root) << endl;
 }
